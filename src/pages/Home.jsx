8b02bc9814697cdd0c975901cfcf7d332c5634a7
Max <massimo.musso@edu-its.it>
2025-11-07 22:42:05 +0100

    Aggiunto loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,18 +18,17 @@ import {
 import HeroBanner from '../components/HeroBanner';
 import MovieRow from '../components/MovieRow';
 import Loader from '../components/Loader';
-import { div } from 'motion/react-client';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { popular, topRated, loading } = useSelector((state) => state.movies)
+  const { popular, topRated, status } = useSelector((state) => state.movies)
 
   useEffect(() => {
     dispatch(fetchPopularMovies())
     dispatch(fetchTopRatedMovies())
   }, [dispatch])
 
-  if (loading) return <Loader />
+  if (status === 'loading') return <Loader />
   return (
     <div className="mt-10 space-y-10">
       {popular.length > 0 && <HeroBanner movie={popular[0]}/>}
